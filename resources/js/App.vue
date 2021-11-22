973305c58b4b09082a827fb7b5a05f8c16fd9c29
FrankLaurora <francesco.laurora9@gmail.com>
2021-11-22 18:27:52 +0100

    Add post list to /api-posts

---
+++ resources/js/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <Header/>
+        <Main :posts="posts"/>
+    </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Main from './components/Main.vue'
+
+    export default {
+        name: 'App',
+
+        components: {
+            Header,
+            Main
+        },
+
+        data() {
+            return {
+                'posts' : []
+            }
+        },
+
+        mounted () {
+            axios.get('http://localhost:8000/api/posts')
+                .then(response => {
+                    this.posts = response.data.data;
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        }
+    }
+</script>
---
+++ resources/js/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+    <main>
+        <section>
+            <h2>Tutti i post</h2>
+            <ul>
+                <li v-for="post in posts" :key="post.id">{{post.title}}</li>
+            </ul>
+        </section>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+
+    props: {
+        posts: Array
+    }
+}
+</script>
